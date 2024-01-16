c969faeac26372610106b4d1ad270691c7211677
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-01-16 10:50:51 +0300

    Add files via upload

---
+++ picturebox_buton/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace picturebox_buton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Image = global::picturebox_buton.Properties.Resources._004_squirrel;
+            this.button4.Location = new System.Drawing.Point(55, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 72);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::picturebox_buton.Properties.Resources._003_turtle;
+            this.button3.Location = new System.Drawing.Point(55, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 72);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::picturebox_buton.Properties.Resources._002_snail;
+            this.button2.Location = new System.Drawing.Point(55, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 72);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::picturebox_buton.Properties.Resources._001_easter_bunny;
+            this.button1.Location = new System.Drawing.Point(55, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 72);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 405);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ picturebox_buton/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picturebox_buton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            button1.Left= 0;
+            button2.Left = 0;
+            button3.Left = 0;
+            button4.Left = 0;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            button1.Left = button1.Left + 1 + random.Next(1, 30);
+            button2.Left = button2.Left + 1 + random.Next(1, 30);
+            button3.Left = button3.Left + 1 + random.Next(1, 30);
+            button4.Left = button4.Left + 1 + random.Next(1, 30);
+        }
+    }
+}
